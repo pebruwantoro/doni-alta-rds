2114d635f90b841f90dcef951d6ac5288fcbbb34
Doni Pebruwantoro <pebruwantoro.doni@gmail.com>
2021-08-10 23:26:41 +0700

    first commit

---
+++ program/main.go
@@ -0,0 +1,56 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"github.com/labstack/echo"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var DB *gorm.DB
+
+type User struct {
+	gorm.Model
+	Name string
+}
+
+func main() {
+	connectionString := os.Getenv("CONNECTION_STRING")
+	var err error
+	DB, err = gorm.Open(mysql.Open(connectionString), &gorm.Config{})
+	if err != nil {
+		panic(err)
+	}
+	DB.AutoMigrate(&User{})
+	port := os.Getenv("HTTP_PORT")
+	if port == "" {
+		port = ":80"
+	}
+	e := echo.New()
+	e.GET("/", hello)
+	e.GET("/user", getUser)
+	if err := e.Start(port); err != nil {
+		fmt.Println(err)
+	}
+}
+
+func hello(c echo.Context) error {
+	return c.String(200, "hello")
+}
+
+func helloName(c echo.Context) error {
+	name := c.Param("name")
+	return c.String(200, fmt.Sprintf("Hello %s", name))
+}
+
+func getUser(c echo.Context) error {
+	var users []User = []User{}
+	if err := DB.Find(&users).Error; err != nil {
+		fmt.Println(err)
+		return c.String(500, err.Error())
+	}
+	return c.JSON(http.StatusOK, users)
+}
